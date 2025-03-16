ee9da04e93a457c4d40d9230688fe66b644f7c2a
Prajwal-W <84848155+Prajwal-W@users.noreply.github.com>
2025-03-16 15:40:13 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Prajwal.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">2 years</span> of experience. I enjoy
+        <span className="font-bold">3+ years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React and Node.JS. </span>
       </motion.h1>
